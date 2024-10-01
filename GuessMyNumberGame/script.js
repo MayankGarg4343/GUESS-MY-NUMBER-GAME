7f32dbb0b841674474d3fd882cbfe854090b76b4
Mayank Garg <151990706+MayankGarg4343@users.noreply.github.com>
2024-10-01 14:24:13 +0530

    Update script.js

--- GuessMyNumberGame/script.js
+++ GuessMyNumberGame/script.js
@@ -1,48 +1,3 @@
-'use strict';
-
-/*
-
-
-console.log(document.querySelector('.message').textContent);
-
-// 👉 here the (document.queryselector) points to the element of the html and then we have used the 
-//     (.textcontent) property which is being used to get or set the value.
-
-
-                    //👉   DOM AMD DOM MANIPULATION
-
-// what does dom actually means. the dom stands for the (document object model) it is the structured representation of html document.
-// allows the javascript to access the elements of the html elements and styles to manipulate them.
-
-// manipulate here refers that we can change the text, attributes and even css style.
-
-// dom is alredy created by the borwser as the html page loads. and it used to store it in the form of the tree.
-// dom is the complete representation of the html element.
-
-// actually the dom methods and the properties of the dom manipulation are not the parts of the javascript.
-
-// 👉 then the question here arises that how we access them through the javascript the answer is that they are the part of the web api's
-//     and these api's are the part of the javascript and by this we can access then through the javascript and the api stands for (application programming interface).
-
- document.querySelector('.message').textContent = '🎉 correct number';
-
-// by using this we can change the name of the selected element and it also worked.
-// here we have actually done the dom manipulation. 
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-
-
-
-*/
-
-//👉 handelling the click events.
-
-// aab ek unique number define karege taki hamari game chal sake.
-
 let secretnumber = Math.trunc(Math.random() * 20) + 1;    // selecting the unique number always.
 let score = 10;
 let highscore = 0;
@@ -99,4 +54,3 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
-// yaha jo function hai who event handler ka kaam kar raha hai. 
